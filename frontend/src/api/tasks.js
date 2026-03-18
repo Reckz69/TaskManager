82ae5a274f87ed23e0023380b78919bdca7c0efc
Reckz69 <narendrameshram182@gmail.com>
2026-03-18 15:22:01 +0530

    added backend deployed site

--- frontend/src/api/tasks.js
+++ frontend/src/api/tasks.js
@@ -1,7 +1,7 @@
 import axios from "./axios";
 
 const API = axios.create({
-    baseURL: "https://taskmanager-u7vk.onrender.com/api/v1",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
   });
 
